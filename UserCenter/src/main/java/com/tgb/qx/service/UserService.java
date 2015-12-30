3c8968b684703eb708b65904ba8511b4a0e4dbb9
wangzhipeng <wangzhipeng@acfun.tv>
2015-12-30 18:04:33 +0800

    add module UserCenter

---
+++ UserCenter/src/main/java/com/tgb/qx/service/UserService.java
@@ -0,0 +1,11 @@
+package com.tgb.qx.service;
+
+/**
+ * Created by wzp on 15/12/30.
+ */
+public interface UserService {
+    public void addUser();
+    public void updateUser();
+    public void delUser();
+    public void getUser();
+}
